cfc87b6edb61d8743ac9701f5fb85f81c6d86e58
Mathias <mathiasbdev@gmail.com>
2024-12-09 09:58:48 -0300

    report print template design

--- src/app/admin/view-pdf-report/[userId]/page.tsx
+++ src/app/admin/view-pdf-report/[userId]/page.tsx
@@ -3,14 +3,13 @@ import { useEffect, useState } from 'react';
 import axios from 'axios';
 import { ReportUser } from '@/utils/types';
 import jsPDF from 'jspdf';
+import ReportPrintTemplate from '@/components/reportList/ReportPrintTemplate';
+import { LeftOutlined, RightOutlined } from '@ant-design/icons';
 
 const ViewPdfReport = ({ params }: { params: { userId: string } }) => {
     const { userId } = params;
     const [loading, setLoading] = useState(false);
     const [reportUser, setReportUser] = useState<ReportUser | null>(null);
-
-    const images = ['/pdf1.jpg', '/pdf2.jpg']; // Rutas de tus imágenes
-    const [currentImage, setCurrentImage] = useState(0); // Estado para el slider
 
     useEffect(() => {
         setLoading(true);
@@ -32,36 +31,10 @@ const ViewPdfReport = ({ params }: { params: { userId: string } }) => {
     }, [userId]);
 
     const downloadPDF = () => {
-        const doc = new jsPDF({
-            orientation: 'portrait',
-            unit: 'px',
-            format: [794, 1123], // Tamaño de la imagen en píxeles
-        });
-
-        // Cargar la primera imagen
-        const img1 = new Image();
-        img1.src = '/pdf1.jpg'; // Ruta de tu primera imagen
-        img1.onload = () => {
-            doc.addImage(img1, 'JPEG', 0, 0, 794, 1123);
-
-            // Cargar la segunda imagen
-            const img2 = new Image();
-            img2.src = '/pdf2.jpg'; // Ruta de tu segunda imagen
-            img2.onload = () => {
-                // Agregar una nueva página para la segunda imagen
-                doc.addPage();
-                doc.addImage(img2, 'JPEG', 0, 0, 794, 1123);
-
-                // Descargar el PDF con ambas imágenes
-                doc.save('reporte.pdf');
-            };
-        };
+        
     };
 
-    // Función para cambiar entre las imágenes y actualizar el estado de la imagen activa
-    const handleImageChange = (index: number) => {
-        setCurrentImage(index);
-    };
+    const [ page, setPage ] = useState(1);
 
     return (
         loading ? (
@@ -71,36 +44,29 @@ const ViewPdfReport = ({ params }: { params: { userId: string } }) => {
         ) : (
             <div className="relative w-full h-screen overflow-hidden flex flex-col justify-center items-center bg-gray-200">
                 {/* Contenedor de la imagen */}
-                <div
-                    className="relative"
-                    style={{
-                        width: '794px', // Ancho de la imagen en píxeles
-                        height: '1123px', // Alto de la imagen en píxeles
-                        backgroundImage: `url(${images[currentImage]})`, // Usamos el estado para cambiar la imagen
-                        backgroundSize: 'contain',
-                        backgroundPosition: 'center',
-                        backgroundRepeat: 'no-repeat',
-                    }}
-                >
-                    {/* Texto posicionado sobre la imagen */}
-                    <div className="absolute top-10 left-10 text-white bg-black bg-opacity-50 p-4 rounded">
-                        Este es un texto que aparece sobre la imagen.
-                    </div>
-
-                    {/* Botones de navegación del slider */}
-                    <button
-                        onClick={() => handleImageChange(currentImage === 0 ? 1 : 0)} // Cambia entre imagen 1 y 2
-                        className="absolute left-4 top-1/2 transform -translate-y-1/2 bg-blue-500 text-white p-2 rounded hover:bg-blue-600"
+                <div className={'relative h-full'}>
+                    <button 
+                        className={'absolute top-1/2 -translate-y-1/2 -left-11 disabled:text-neutral-400 text-2xl'} 
+                        disabled={page === 1}
+                        onClick={() => setPage(1)}
+                    > 
+                        <LeftOutlined  />
+                    </button>
+                    <ReportPrintTemplate page={page} />
+                    <button 
+                        className={'absolute top-1/2 -translate-y-1/2 -right-11 disabled:text-neutral-400 text-2xl'} 
+                        disabled={page === 2}
+                        onClick={() => setPage(2)}
                     >
-                        {currentImage === 0 ? 'Ver Imagen 2' : 'Ver Imagen 1'}
+                        <RightOutlined  />
                     </button>
                 </div>
 
                 {/* Botón para descargar PDF */}
-                <div className="absolute bottom-10 flex gap-4">
+                <div className="absolute bottom-5 flex gap-4">
                     <button
                         onClick={downloadPDF}
-                        className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600"
+                        className="flex items-center gap-2 px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600"
                     >
                         Descargar como PDF
                     </button>
---
+++ src/components/reportList/ReportPrintTemplate.tsx
@@ -0,0 +1,162 @@
+import Image from "next/image";
+
+export default function ReportPrintTemplate({ page }: { page: number }) {
+    return page === 1 ? <Page1/> : <Page2/>;
+}
+
+function Page1() {
+    return (
+        <div className={'flex flex-col gap-8 bg-white aspect-[1/1.414] h-full p-10 text-[.65rem] font-medium select-none'}>
+            <div className={'flex items-center justify-center gap-8'}>
+                <Image src={'/senasa.png'} alt={'senasa'} width={45} height={50} />
+                <div className={'flex flex-col divide-y'}>
+                    <div className={'flex items-center border-black'}>
+                        <span className={'font-bold w-14'}>Titulo</span>
+                        <span>Formato de historia clínica para la preautorización de procedimientos</span>
+                    </div>
+                    <div className={'flex items-center border-black'}>
+                        <span className={'font-bold w-14'}>Código</span>
+                        <span>GSS-FORM-078</span>
+                    </div>
+                    <div className={'flex items-center border-black'}>
+                        <span className={'font-bold w-14'}>Versión</span>
+                        <div className={' grid grid-cols-2 divide-x'}>
+                            <span>00</span>
+                            <span className={'pl-1 border-black'}>Fecha de aprobación: 02/12/2021</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className={'flex flex-col border border-black'}>
+                <div className={'flex items-end justify-end p-1 pb-5 border-b border-black'}>
+                    <span className={'font-bold leading-3'}>Fecha de solicitud:</span>
+                    <div className={'w-40 border-b border-black'}></div>
+                </div>
+                <div className={'flex flex-col border-black divide-y'}>
+                    <div className={'flex justify-between border-black'}>
+                        <div className={''}>
+                            <div className={'font-semibold bg-black text-white p-1 w-[7.8rem]'}>Nombre del médico:</div>
+                        </div>
+                        <div className={'flex'}>
+                            <div className={'font-semibold bg-black text-white p-1 w-24'}>Código:</div>
+                            <div className={'w-24'}></div>
+                        </div>
+                    </div>
+                    <div className={'flex justify-between border-black'}>
+                        <div className={''}>
+                            <div className={'font-semibold bg-black text-white p-1 w-[7.8rem]'}>Nombre del afiliado:</div>
+                        </div>
+                        <div className={'flex'}>
+                            <div className={'font-semibold bg-black text-white p-1 w-24'}>NSS:</div>
+                            <div className={'w-24'}></div>
+                        </div>
+                    </div>
+                    <div className={'flex justify-between border-black'}>
+                        <div className={''}>
+                            <div className={'font-semibold bg-black text-white p-1 w-[7.8rem]'}>Cédula:</div>
+                        </div>
+                        <div className={'flex'}>
+                            <div className={'font-semibold bg-black text-white p-1 w-24'}>Edad:</div>
+                            <div className={'w-24'}></div>
+                        </div>
+                    </div>
+                    <div className={'flex justify-between border-black'}>
+                        <div className={''}>
+                            <div className={'font-semibold bg-black text-white p-1 w-[7.8rem]'}>Sexo:</div>
+                        </div>
+                        <div className={'flex'}>
+                            <div className={'font-semibold bg-black text-white p-1 w-24'}>Teléfono:</div>
+                            <div className={'w-24'}></div>
+                        </div>
+                    </div>
+                    <div className={'flex border-black'}>
+                        <div className={''}>
+                            <div className={'font-semibold bg-black text-white p-1 pr-7'}>Centro donde se realizará el estudio:</div>
+                        </div>
+                    </div>
+                    <div className={'flex border-black'}>
+                        <div className={''}>
+                            <div className={'font-semibold bg-black text-white p-1 pr-7'}>Centro donde se realiza el procedimiento:</div>
+                        </div>
+                    </div>
+                    <div className={'flex border-black'}>
+                        <div className={''}>
+                            <div className={'font-semibold bg-black text-white p-1 pr-7'}>Accidente de tránsito:</div>
+                        </div>
+                        <div className={'flex divide-x border-r border-black'}>
+                            <span className={'grid place-content-center font-bold w-10 border-black'}>Sí</span>
+                            <span className={'grid place-content-center font-bold w-10 border-black'}></span>
+                            <span className={'grid place-content-center font-bold w-10 border-black'}>No</span>
+                            <span className={'grid place-content-center font-bold w-10 border-black'}></span>
+                        </div>
+                    </div>
+                </div>
+                <div className={'flex flex-col'}>
+                    <div className={'font-semibold bg-black text-white p-1 text-center'}>Diagnóstico (s):</div>
+                    <div className={'p-1'}>
+                        Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto.
+                        Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500,
+                        cuando un impresor (N. del T. persona que se dedica a la imprenta) desconocido usó
+                        una galería de textos y los mezcló de tal manera que logró hacer un libro de textos especimen.
+                    </div>
+                </div>
+                <div className={'flex flex-col'}>
+                    <div className={'font-semibold bg-black text-white p-1 text-center'}>Nombre del (los) procedimiento (s):</div>
+                    <div className={'p-1'}>
+                        1. Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. <br/>
+                        2. Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500,<br />
+                        3. cuando un impresor (N. del T. persona que se dedica a la imprenta) desconocido usó<br />
+                        4. una galería de textos y los mezcló de tal manera que logró hacer un libro de textos especimen.<br />
+                    </div>
+                </div>
+                <div className={'flex flex-col'}>
+                    <div className={'font-semibold bg-black text-white p-1 text-center'}>Historia de la enfermedad actual:</div>
+                    <div className={'p-1'}>
+                        Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto.
+                        Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500,
+                        cuando un impresor (N. del T. persona que se dedica a la imprenta) desconocido usó
+                        una galería de textos y los mezcló de tal manera que logró hacer un libro de textos especimen.
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+function Page2() {
+    return (
+        <div className={'flex flex-col gap-8 bg-white aspect-[1/1.414] h-full p-10 text-[.65rem] font-medium select-none'}>
+            <div className={'flex items-center justify-center gap-8'}>
+                <Image src={'/senasa.png'} alt={'senasa'} width={45} height={50} />
+                <div className={'flex flex-col divide-y'}>
+                    <div className={'flex items-center border-black'}>
+                        <span className={'font-bold w-14'}>Titulo</span>
+                        <span>Formato de historia clínica para la preautorización de procedimientos</span>
+                    </div>
+                    <div className={'flex items-center border-black'}>
+                        <span className={'font-bold w-14'}>Código</span>
+                        <span>GSS-FORM-078</span>
+                    </div>
+                    <div className={'flex items-center border-black'}>
+                        <span className={'font-bold w-14'}>Versión</span>
+                        <div className={' grid grid-cols-2 divide-x'}>
+                            <span>00</span>
+                            <span className={'pl-1 border-black'}>Fecha de aprobación: 02/12/2021</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className={'flex flex-col border border-black'}>
+                <div className={'flex flex-col'}>
+                    <div className={'font-semibold bg-black text-white p-1 text-center'}>Antecedente patológico:</div>
+                    <div className={'p-1'}>
+                        Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto.
+                        Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500,
+                        cuando un impresor (N. del T. persona que se dedica a la imprenta) desconocido usó
+                        una galería de textos y los mezcló de tal manera que logró hacer un libro de textos especimen.
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
